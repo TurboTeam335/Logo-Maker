4e6c32e4e59bc8d11ebf366bf612a0a276eed7d6
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-04-27 15:35:23 -0500

    test

---
+++ Develop/index.js
@@ -0,0 +1,3 @@
+const CLI = require("../Develop/lib/cli");
+
+new CLI().run();
---
+++ Develop/lib/cli.js
@@ -0,0 +1,68 @@
+const inquirer = require("inquirer");
+const SVG = require("../lib/svg");
+const { Circle, Triangle, Square } = require("../lib/shapes");
+const { writeFileSync } = require("fs");
+
+class CLI {
+  async run() {
+    const { text } = await inquirer.prompt({
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters for the logo text:",
+      validate: function (input) {
+        if (input.length > 3) {
+          return "Text must not exceed 3 characters.";
+        }
+        return true;
+      },
+    });
+
+    const { textColor } = await inquirer.prompt({
+      type: "list",
+      name: "textColor",
+      message: "Select a color for the text",
+      choices: ["red", "blue", "green", "yellow"],
+    });
+
+    const { shape } = await inquirer.prompt({
+      type: "list",
+      name: "shape",
+      message: "Choose a shape for the logo:",
+      choices: ["circle", "triangle", "square"],
+    });
+
+    const { shapeColor } = await inquirer.prompt({
+      type: "list",
+      name: "shapeColor",
+      message: "Select a color for the text",
+      choices: ["red", "blue", "green", "yellow"],
+    });
+
+    let shapeObj;
+    switch (shape) {
+      case "circle":
+        shapeObj = new Circle();
+        break;
+      case "triangle":
+        shapeObj = new Triangle();
+        break;
+      case "square":
+        shapeObj = new Square();
+        break;
+      default:
+        throw new Error(`Invalid shape: ${shape}`);
+    }
+    shapeObj.setColor(shapeColor);
+    console.log(shapeObj)
+    const svg = new SVG(text, textColor, shapeObj.render, shapeObj.color);
+    console.log(svg)
+
+    const svgMarkup = svg.render();
+
+    await writeFileSync("logo.svg", svgMarkup);
+
+    console.log("Generated logo.svg");
+  }
+}
+
+module.exports = CLI;
